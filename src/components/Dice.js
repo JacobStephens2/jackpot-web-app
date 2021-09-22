d57966cd4b1111cf50c5607a716724a89c5c8960
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-21 23:19:42 -0400

    add rollAverage state to dice component

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,71 +1,111 @@
 import * as React from 'react'
-import {useState} from 'react';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography'
 
+class Dice extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = {
+			rollResult : '',
+			rollHistoryArray: [],
+			rollHistoryString: '',
+			rollAverage: ''
+	  }
+	  this.rollDice = this.rollDice.bind(this);
+	}
 
-const Dice = (props) => {
-	const [rollResult, setRollResult] = useState( 0 );
-	const [countOfRolls, setCountOfRolls] = useState( 0 );
-	const [arrayOfRolls, setArrayOfRolls] = useState([]);
-	const [stringOfRolls, setStringOfRolls] = useState( '' );
-	const [averageRoll, setAverageRoll] = useState( 0 );
-	const [sumOfRolls, setSumOfRolls] = useState( 0 );
-
-	function rollDice() {
-		var result = (Math.floor(Math.random() * (props.diceSidesCount - 1 + 1)) + 1);
+	rollDice() {
+		var result = (Math.floor(Math.random() * (this.props.diceSidesCount - 1 + 1)) + 1);
 		var i = 1;
-		while (i < props.diceCount) {
-			result = result + (Math.floor(Math.random() * (props.diceSidesCount - 1 + 1)) + 1);
+		while (i < this.props.diceCount) {
+			result = result + (Math.floor(Math.random() * (this.props.diceSidesCount - 1 + 1)) + 1);
 			i++;
 		}
-		findAverage(result);
-		setCountOfRolls(countOfRolls + 1);
-		addToRollsList(result);
-		setRollResult(result);
-		setArrayOfRolls(arrayOfRolls => [...arrayOfRolls, result]);
-		console.log("countOfRolls: " + countOfRolls + ', ' + typeof countOfRolls);
-		console.log("stringOfRolls: " + stringOfRolls + ', ' + typeof stringOfRolls);
-		console.log("averageRoll: " + averageRoll + ', ' + typeof averageRoll);
-		console.log("rollResult: " + rollResult + ', ' + typeof rollResult);
-		console.log("sumOfRolls: " + sumOfRolls + ', ' + typeof sumOfRolls);
-		console.log("arrayOfRolls: " + arrayOfRolls + ', ' + typeof arrayOfRolls);				
-	}
-	
-	function addToRollsList(result) {
-		if(countOfRolls === 0) {
-			setStringOfRolls(arrayOfRolls.concat(result));
+		this.setState({
+			rollResult: result
+		})
+		this.setState({
+			rollHistoryArray: this.state.rollHistoryArray.concat(result)
+		}, () => {
+			this.findAverage();
+		})
+		if (this.state.rollHistoryString === '') {
+			this.setState({
+				rollHistoryString: result
+			})
 		} else {
-			setStringOfRolls(stringOfRolls.concat(', ' + result));
+			this.setState({
+				rollHistoryString: this.state.rollHistoryString + ', ' + result
+			})
 		}
 	}
 
-	function findAverage(result) {
-		setSumOfRolls( sumOfRolls + result );
-		setAverageRoll(  sumOfRolls / countOfRolls );
+	findAverage() {
+		var total = 0;
+		for(var i = 0; i < this.state.rollHistoryArray.length; i++) {
+			total += this.state.rollHistoryArray[i];
+		}
+		this.setState({
+			rollAverage: (total / this.state.rollHistoryArray.length).toFixed(1)
+		})
 	}
 
-	return (
+
+	render() {
+	  return (
 		<div>
-			{props.diceCount}D{props.diceSidesCount} roll result: {rollResult}
-			<br />
-			Count of dice rolls: {countOfRolls}
-			<br />
-			Dice rolls: {stringOfRolls}
-			<br />
-			Average roll: {averageRoll}
-			<br />
+			<Typography variant="body1">
+				Roll result: {this.state.rollResult}
+			</Typography>
+			<Typography variant="body1">
+				Roll history: {this.state.rollHistoryString}
+			</Typography>
+			<Typography variant="body1">
+				Roll average: {this.state.rollAverage}
+			</Typography>
+
 			<Button 
 				variant="contained"
 				onClick={() => {
-					rollDice();
+					this.rollDice();
 				}}
 			>
-				Roll Dice
+				Roll {this.props.diceCount}D{this.props.diceSidesCount}
+			</Button>
+			<br />
+			
+			{/* Test states button */}
+			<Button 
+				variant="contained"
+				onClick={() => {
+					console.log(
+						"rollResult: " + this.state.rollResult + 
+						': ' + typeof this.state.rollResult + 
+						': Array? ' + Array.isArray(this.state.rollResult)
+					);
+					console.log(
+						"rollHistoryArray: " + this.state.rollHistoryArray + 
+						': ' + typeof this.state.rollHistoryArray +
+						': Array? ' + Array.isArray(this.state.rollHistoryArray) +
+						': ArrayLength? ' + this.state.rollHistoryArray.length
+					);
+					console.log(
+						"rollHistoryString: " + this.state.rollHistoryString + 
+						': ' + typeof this.state.rollHistoryString +
+						': Array? ' + Array.isArray(this.state.rollHistoryString)
+					);
+					console.log(
+						"rollAverage: " + this.state.rollAverage + 
+						': ' + typeof this.state.rollAverage +
+						': Array? ' + Array.isArray(this.state.rollAverage)
+					);
+				}}
+			>
+				Log states
 			</Button>
 		</div>
-	)
-}
+	  );
+	}
+  };
 
 export default Dice
-
-
--- src/pages/jackpot.js
+++ src/pages/jackpot.js
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import Typography from '@mui/material/Typography'
-import DiceClass from '../components/DiceClass.js'
+import Dice from '../components/Dice.js'
 
 const Jackpot = () => {
 	return (
 		<Layout pageTitle="Jackpot">
 			<Typography variant="body1" component="div" gutterBottom>
 				Roll the dice to see the results.
-				<DiceClass diceCount="2" diceSidesCount="6" />
+				<Dice diceCount="2" diceSidesCount="6" />
 			</Typography>
 		</Layout>
 	)
