a8ed99b065c030416961a886d4f246dadf486ce1
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-10-01 18:33:54 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
     title: "The Well Played Game",
   },
   plugins: [
-    'gatsby-plugin-material-ui',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-material-ui",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -21,6 +21,25 @@ module.exports = {
         },
       },
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZTG31WK4NH", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
 };
